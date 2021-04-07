85c57b3cb00894ab8d62f5543651a9501a12a4ca
unknown <sagemuho@gmail.com>
2021-04-07 11:55:36 +0200

    added put,patch and delete

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -32,3 +32,16 @@ class HelloApiView(APIView):
             return Response(
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        """handle updating an object"""
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        """Handle a partial update of an object """
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
+        
